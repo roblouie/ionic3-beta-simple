7e7384e557939169b16d2f8f6a85ffe33f46ed67
roblouie <rob.j.louie@gmail.com>
2017-03-28 11:34:54 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { MyApp } from './app.component';
+import { HomePageModule } from '../pages/home/home.module';
+
+@NgModule({
+  declarations: [
+    MyApp,
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HomePageModule
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/second-page/second-page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+import {IonicPage} from "ionic-angular/index";
+
+@IonicPage()
+@Component({
+  selector: 'second-page',
+  templateUrl: 'second-page.html'
+})
+export class SecondPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
